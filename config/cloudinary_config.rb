aee4de04b42fb81185c1af3ab1558ab9281756aa
tsofitvail20 <62537985+tsofitvail20@users.noreply.github.com>
2020-04-19 20:46:57 +0300

    Update cloudinary_config.rb

--- config/cloudinary_config.rb
+++ config/cloudinary_config.rb
@@ -1,6 +1,6 @@
 
 Cloudinary.config do |config|
-  config.cloud_name = "tsofit"
-  config.api_key = "791653976245367"
-  config.api_secret = "ITOWffdT-XTh6jaVfc4I-8zcNiE"
+  config.cloud_name =""
+  config.api_key = ""
+  config.api_secret =""
 end
